e707bbf195b0a69114641db0b8df9385058b7cae
Deepspace <cxin1427@gmail.com>
2019-03-09 19:35:01 +0800

    add switch component

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Switch from '../Switch';
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='switch'>
+        <Switch />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Switch/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class componentName extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      isChecked: false,
+    };
+  }
+
+  componentWillMount() {
+    const { isChecked } = this.props;
+    this.setState({ isChecked });
+  }
+
+  handleChange(event) {
+    this.setState({ isChecked: event.target.checked });
+    console.log(`checked: ${event.target.checked}`);
+  }
+
+  render() {
+    return (
+      <div className='switchContainer'>
+        <label>
+          <input
+            checked={this.state.isChecked}
+            onChange={this.handleChange}
+            className='switch'
+            type='checkbox'
+          />
+          <div>
+            <div />
+          </div>
+        </label>
+      </div>
+    );
+  }
+}
+
+export default componentName;
